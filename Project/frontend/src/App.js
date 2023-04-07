fb59e33afac64b688844b94ecf63b4f9400fa01d
K L Shashin Kalpajith <127762218+Shashin99@users.noreply.github.com>
2023-04-07 12:54:04 +0530

    set up environment

---
+++ Project/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+
+
+	return (
+		<div className="App">
+			<div className="App-header">
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ Project/frontend/src/App.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+	// eslint-disable-next-line react/react-in-jsx-scope
+	render(<App />);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
+});
