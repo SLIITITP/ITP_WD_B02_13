508dc0e39d9b319e8dcf35d8f1324b6190c006fe
Ludeesha Anjalee <ludeeshaa@gmail.com>
2023-04-16 02:20:52 +0530

    update

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -1,32 +1,24 @@
-const express = require ("express");
-const mongoose = require ("mongoose");
-const bodyParser = require ("body-parser");
-const cors = require ("cors");
-const dotenv = require ("dotenv");
+const express = require("express");
 const app = express();
-require("dotenv").config();
+const cors = require("cors");
+require("dotenv").config({ path: "./.env" });
 
 const PORT = process.env.PORT || 8070;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-const URL = process.env.MONGODB_URL;
+//get database connection
+const dbo = require("./db/conn");
 
-/* Connect to the Database */
-mongoose.connect(URL, {
-    // useCreateIndex: true, 
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-    // useFindAndModify: false
-});
+//get routes files
 
-const connection = mongoose.connection;
-connection.once ("open" , () => {
-    console.log("MongoDB Connection success!");
-})
 
 app.listen(PORT, () => {
-    console.log(`Server is up and running on port number: ${PORT}`)
-})
-
+	//perform connection to database
+	dbo.connectToServer(function (err) {
+		if (err) console.error(err);
+	});
+	console.log("................");
+	console.log(`Server is up and running on port ${PORT}`);
+});
