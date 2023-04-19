587c8fb6c2b5ac3b3b0f47288811d30bb8b6fe70
K L Shashin Kalpajith <127762218+Shashin99@users.noreply.github.com>
2023-04-19 23:04:24 +0530

    implemet CRUD

--- Project/backend/routes/feedback/record.js
+++ Project/backend/routes/feedback/record.js
@@ -18,7 +18,30 @@ feedbackRoutes.route("/add").post(function (req, response) {
 
 	db_connect.collection("feedback").insertOne(myobject, function (err, res) {
 		if (err) throw err;
+
 		console.log("1 record inserted");
+		response.json(res);
+	});
+});
+
+// http://localhost:8070/feedback/update/:id ( update a record by id )
+feedbackRoutes.route("/update/:id").post(function (req, response) {
+
+	let db_connect = dbo.getDb("sansalu");
+
+	let myquery = { _id: ObjectId(req.params.id) };
+
+	let newvalues = {
+		$set: {
+			name:req.body.name,
+            description:req.body.description
+		},
+	};
+
+	db_connect.collection("feedback").updateOne(myquery, newvalues, function (err, res) {
+		if (err) throw err;
+        
+		console.log("1 record updated");
 		response.json(res);
 	});
 });
