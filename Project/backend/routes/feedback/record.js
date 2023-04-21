596d38862d7489656882c384f59d26c2557f9c57
K L Shashin Kalpajith <127762218+Shashin99@users.noreply.github.com>
2023-04-21 12:13:49 +0530

    implement CRUD

--- Project/backend/routes/feedback/record.js
+++ Project/backend/routes/feedback/record.js
@@ -7,6 +7,32 @@ const dbo = require("../../db/conn"); // connect to the database
 const ObjectId = require("mongodb").ObjectId // convert the Id from String to ObjectId for the _id
 
 
+// http://localhost:8070/feedback/ ( get all record )
+feedbackRoutes.route("/").get(function (req, res) {
+
+	let db_connect = dbo.getDb("sansalu");
+
+	db_connect.collection("feedback").find({}).toArray(function (err, result) {
+			if (err) throw err;
+
+			res.json(result);
+		});
+});
+
+// http://localhost:8070/feedback/feedback/:id ( get a single record by id )
+feedbackRoutes.route("/feedback/:id").get(function (req, res) {
+
+	let db_connect = dbo.getDb("sansalu");
+
+	let myquery = { _id: ObjectId(req.params.id) };
+
+	db_connect.collection("feedback").findOne(myquery, function (err, result) {
+		if (err) throw err;
+
+		res.json(result);
+	});
+});
+
 // http://localhost:8070/feedback/add ( created 1 record )
 feedbackRoutes.route("/add").post(function (req, response) {
 
