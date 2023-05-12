ee3ea966ead1efc36982e78dc6f5a8c49b72ced6
Nimesha Rajapaksha <127377276+NimeshaR@users.noreply.github.com>
2023-05-12 22:43:17 +0530

    navbar and footer

--- Project/frontend/src/components/navbar/navbar.js
+++ Project/frontend/src/components/navbar/navbar.js
@@ -30,7 +30,7 @@ export default function NavBar(){
 										href="/"
 										className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 									>
-										Service
+										Our Services
 									</a>
 								</li>
 								<li>
@@ -46,7 +46,7 @@ export default function NavBar(){
 										href="/"
 										className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 									>
-										Contact
+										Contact Us
 									</a>
 								</li>
                                 <li>
