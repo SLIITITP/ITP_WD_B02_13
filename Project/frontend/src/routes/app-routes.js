140527796a088b91b065977504edb1dc73d25e0d
ShashiniH <shshani0002@gmail.com>
2023-05-16 12:56:25 +0530

    image modification

--- Project/frontend/src/routes/app-routes.js
+++ Project/frontend/src/routes/app-routes.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "../components/navbar/navbar";
 import Footer from "../components/footer/footer";
 
-import{
+import {
 	Login,
 	ClientLogin,
 	AdminLogin,
@@ -23,15 +23,15 @@ import{
 	ViewCus,
 	CusSearch,
 
-  	DesignPortal,
-  	Dtemplate,
-  	Ptype,
-  	Material,
-  	Utemplate,
-  	UprintType,
-  	Umaterial,
-  
-  Checkout,
+	DesignPortal,
+	Dtemplate,
+	Ptype,
+	Material,
+	Utemplate,
+	UprintType,
+	Umaterial,
+
+	Checkout,
 	ViewDetails,
 	OrderAdmin,
 	Invoice,
@@ -46,48 +46,48 @@ function App() {
 				<NavBar />
 
 				<Routes>
-    
-				 {/* Login Selection */}
-				 <Route path="/login" element={<Login />} />
-				 <Route path="/login/clientlogin" element={<ClientLogin />} />
-				 <Route path="/login/adminlogin" element={<AdminLogin />} />
-				 <Route path="/login/emplogin" element={<EmpLogin />} />
 
-				 {/* Register Selection */}
-				 <Route path="/register" element={<Register/>} />
-				 <Route path="/register/clientregister" element={<RegClient/>} />
+					{/* Login Selection */}
+					<Route path="/login" element={<Login />} />
+					<Route path="/login/clientlogin" element={<ClientLogin />} />
+					<Route path="/login/adminlogin" element={<AdminLogin />} />
+					<Route path="/login/emplogin" element={<EmpLogin />} />
 
-				 {/* Client Dashboard */}
-				 <Route path="/clientdash" element={<ClientDash />} />
-				 <Route path="/clientdash/clientupdate/:id" element={<ClientUpdate/>}/>
-				 <Route path="/clientdash/clientmore/:id" element={<ClientMore/>}/>
-				 <Route path="/clientdash/addloyalty" element={<AddLoyalty />} />
-				 <Route path="/clientdash/searchcus/:key" element={<CusSearch/>}/>
-				 
-				 {/*  Admin Dashboard*/}
-				 <Route path="/admindash" element={<AdminDash />} />	
+					{/* Register Selection */}
+					<Route path="/register" element={<Register />} />
+					<Route path="/register/clientregister" element={<RegClient />} />
+
+					{/* Client Dashboard */}
+					<Route path="/clientdash" element={<ClientDash />} />
+					<Route path="/clientdash/clientupdate/:id" element={<ClientUpdate />} />
+					<Route path="/clientdash/clientmore/:id" element={<ClientMore />} />
+					<Route path="/clientdash/addloyalty" element={<AddLoyalty />} />
+					<Route path="/clientdash/searchcus/:key" element={<CusSearch />} />
+
+					{/*  Admin Dashboard*/}
+					<Route path="/admindash" element={<AdminDash />} />
 
 
-				 {/* Client Management */}
-				 <Route path="/manageclient" element={<ClientManagement />} />
-				 <Route path="/viewallcus" element={<ViewAllCus />} />
-			 	 <Route path="/viewcus/:id" element={<ViewCus />} />
-				 
-         		<Route path="/portal" element = {<DesignPortal/>}/>
-				 <Route path="/template" element = {<Dtemplate/>}/>
-				 <Route path="/print" element = {<Ptype/>}/>
-				 <Route path="/material" element = {<Material/>}/>
-				 <Route path="/updateTemplate/:id" element = {<Utemplate/>}/>
-				 <Route path="/updatePrintType/:id" element = {<UprintType/>}/>
-				 <Route path="/updateMaterial/:id" element = {<Umaterial/>}/>
-           
-          <Route path="/checkout" element={<Checkout />} />
+					{/* Client Management */}
+					<Route path="/manageclient" element={<ClientManagement />} />
+					<Route path="/viewallcus" element={<ViewAllCus />} />
+					<Route path="/viewcus/:id" element={<ViewCus />} />
+
+					<Route path="/portal" element={<DesignPortal />} />
+					<Route path="/template" element={<Dtemplate />} />
+					<Route path="/print" element={<Ptype />} />
+					<Route path="/material" element={<Material />} />
+					<Route path="/updateTemplate/:id" element={<Utemplate />} />
+					<Route path="/updatePrintType/:id" element={<UprintType />} />
+					<Route path="/updateMaterial/:id" element={<Umaterial />} />
+
+					<Route path="/checkout" element={<Checkout />} />
 					<Route path="/ViewDetails/:id" element={<ViewDetails />} />
 					<Route path="/OrderAdmin" element={<OrderAdmin />} />
 					<Route path="/invoice/:id" element={<Invoice />} />
 					<Route path="/AdminReport" element={<AdminReport />} />
 
-					<Route path="/company/:id" element={<Addcompany />} />
+
 				</Routes>
 
 				<Footer />
