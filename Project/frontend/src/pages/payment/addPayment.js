bb8e251bbe17a6bff1987f108c72ec7263690b67
Nimesha Rajapaksha <127377276+NimeshaR@users.noreply.github.com>
2023-05-19 11:24:39 +0530

    report

--- Project/frontend/src/pages/payment/addPayment.js
+++ Project/frontend/src/pages/payment/addPayment.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
 import "./payments.css";
 import Swal from "sweetalert2";
+import { useParams } from "react-router-dom";
 
 export default function AddPayments() {
-
 	const navigate = useNavigate();
 
 	const [TotalAmount, setTotalAmount] = useState("");
@@ -14,6 +14,35 @@ export default function AddPayments() {
 	const [ContactNumber, setContactNumber] = useState("");
 	const [Date, setDate] = useState("");
 	const [Purpose, setPurpose] = useState("");
+
+	const [invoice, setInvoice] = useState("");
+	const [orderDetails, setOrderDetails] = useState([]);
+	const { id } = useParams();
+
+	useEffect(() => {
+		async function fetchOrder() {
+			console.log(id);
+			try {
+				const response = await axios.get("http://localhost:8070/order/getLastOrder/");
+				// handle the response data here
+				const Oid = response.data[0]._id;
+				setInvoice(Oid);
+				console.log(Oid);
+
+				// Fetch order details using the orderId
+				const orderDetailsResponse = await axios.get(`http://localhost:8070/order/invoice/${Oid}`);
+				// handle the order details response data here
+				const orderDetails = orderDetailsResponse.data;
+				console.log("Fetching order details...");
+				console.log(orderDetails);
+
+				setOrderDetails(orderDetails);
+			} catch (error) {
+				alert(error);
+			}
+		}
+		fetchOrder();
+	}, []);
 
 	function sentData1(e) {
 		e.preventDefault();
@@ -73,15 +102,17 @@ export default function AddPayments() {
 					<form onSubmit={sentData1}>
 						<div class="mb-6">
 							<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-								Total Amount
-								
+								Total Amount {}
 							</label>
 							<input
 								type="text"
 								id="email"
 								class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-								placeholder="500000"
-								onChange={(e) =>{setTotalAmount(e.target.value)}}
+								defaultValue={orderDetails.payable}
+								readOnly
+								onChange={(e) => {
+									setTotalAmount(e.target.value);
+								}}
 								required
 							/>
 						</div>
@@ -95,7 +126,8 @@ export default function AddPayments() {
 								class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 								placeholder="John Doe"
 								onChange={(e) => {
-									setRecipientName(e.target.value)}}
+									setRecipientName(e.target.value);
+								}}
 								required
 							/>
 						</div>
@@ -112,7 +144,7 @@ export default function AddPayments() {
 								onChange={(e) => {
 									const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e.target.value);
 									if (isValidEmail) {
-										setRecipientEmail(e.target.value)
+										setRecipientEmail(e.target.value);
 										// The email is valid, you can proceed with further actions
 									} else {
 										// The email is not valid, you can show an error message or take appropriate action
@@ -133,7 +165,9 @@ export default function AddPayments() {
 								placeholder="011-5533621"
 								maxLength="10"
 								minLength="10"
-								onChange={(e) => {setContactNumber(e.target.value)}}
+								onChange={(e) => {
+									setContactNumber(e.target.value);
+								}}
 								required
 							/>
 
@@ -146,7 +180,9 @@ export default function AddPayments() {
 									id="date"
 									class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									placeholder="2021-09-20"
-									onChange={(e) => {setDate(e.target.value)}}
+									onChange={(e) => {
+										setDate(e.target.value);
+									}}
 									required
 								/>
 							</div>
@@ -159,10 +195,11 @@ export default function AddPayments() {
 							rows="4"
 							class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 							placeholder="Purpose"
-							onChange={(e) => {setPurpose(e.target.value)}}
+							onChange={(e) => {
+								setPurpose(e.target.value);
+							}}
 							required
 						/>
-
 
 						<br />
 
